f9ae5109817592a5d320f8addd3918582d396694
kvzfly <247463413@qq.com>
2016-03-31 04:50:04 +0800

    this is a testing

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 cd ${WORKSPACE}/src
-docker build -t 192.168.0.15:80/python-redis-demo:${BUILD_NUMBER} .
+docker build -t 192.168.0.190:80/python-redis-demo:${BUILD_NUMBER} .
 
-docker push 192.168.0.15:80/python-redis-demo:${BUILD_NUMBER}
+docker push 192.168.0.190:80/python-redis-demo:${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key 4C4C52BBC5EED9920183  --secret-key qhQNJU9xwhvJu5duysqccbzMgLZT96Fdo4EmYrrK -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --access-key DEFA2428EAA316EA7D98 --secret-key TiXx5PRF28bREQ61qxRcPBXLhVG4TzLNfBvin2hg -p python-redis-demo-build${BUILD_NUMBER} up -d
 
