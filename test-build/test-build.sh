95dacc635d656df23f5ca243eb270c116b10e1d2
kvzfly <247463413@qq.com>
2016-03-31 20:09:52 +0800

    this is a citest3

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -9,5 +9,5 @@ sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key CC75F5AAD975AA74FE76 --secret-key bCQXviNNnXmLQd22YdZ9ZoaxtsKtyyEWC6nE4VcS -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --access-key CFC9E430325D1CAE6975 --secret-key VwMmSnwdqcZQSsBDY1vVEgoSvETBUWVP6hxw5meN -p python-redis-demo-build${BUILD_NUMBER} up -d
 
