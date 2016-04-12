f7d14fa715fa1ec8ac0b1a2883864dc89a0df6e0
yuhuayun <3246068556@qq.com>
2016-04-13 04:13:50 +0800

    haha

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd ${WORKSPACE}/src
-docker build -t 192.168.0.190:80/python-redis-demo:${BUILD_NUMBER} .
+docker build -t 192.168.0.35:80/python-redis-demo:${BUILD_NUMBER} .
 
-docker push 192.168.0.190:80/python-redis-demo:${BUILD_NUMBER}
+docker push 192.168.0.35:80/python-redis-demo:${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
