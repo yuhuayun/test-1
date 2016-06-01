433adddd1669db55b7edff339790af9e8253d125
yuhuayun <3246068556@qq.com>
2016-06-01 12:45:15 -0400

    2016.6.2

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd ${WORKSPACE}/src
-docker build -t 192.168.0.35:80/python-redis-demo:${BUILD_NUMBER} .
+docker build -t 192.168.5.17:5002/admin/python-with-flask-redis:${BUILD_NUMBER} .
 
-docker push 192.168.0.35:80/python-redis-demo:${BUILD_NUMBER}
+docker push 192.168.5.17:5002/admin/python-with-flask-redis:${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key B81DADD399EBAEE87209 --secret-key ic2x2zhYiYPTriCfLZmpConheJRbraguq47mtxLk -p python-redis-demo-build4 up --pull -d --confirm-upgrade --upgrade pyapp
+./rancher-compose --access-key 109A9CB5565FA0990DE0 --secret-key 2myabxx5kHuEg7eQk9gcSnKeudcWepMxJJfRDpZv -p python-redis-demo-build4 up --pull -d --confirm-upgrade --upgrade pyapp
